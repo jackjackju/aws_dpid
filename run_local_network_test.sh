679b414f50128523bdb7389feb4b2e8df25060f1
Tianyi Zhang <tzha3530@sydney.edu.au>
2024-01-24 15:11:54 +1100

    add size

--- run_local_network_test.sh
+++ run_local_network_test.sh
@@ -9,7 +9,7 @@ killall python3
 i=0
 while [ "$i" -lt $1 ]; do
     echo "start node $i..."
-    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --S 100  --P "dumbo" --D True --O True &
+    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 &
     #python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --S 100 --P "ng" --D True --O True --C $4 &
     # python3 run_sockets_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --S 100  --P "dl" --D True --O True &
 
